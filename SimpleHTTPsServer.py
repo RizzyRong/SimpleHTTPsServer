2fb9a5bcf03ec45564cc8863daebf067fcf6c901
Trevor Christiansen <trevor@tjc.im>
2020-10-09 10:30:45 -0600

    Updated to work with python3
    Fixed the certs

--- SimpleHTTPsServer.py
+++ SimpleHTTPsServer.py
@@ -1,8 +1,8 @@
 # SimpleHTTPsServer, A SimpleHTTPServer that listens on 443, and captures GET/POST requests
 # I recommend using certbot to create a valide certificate.
 
-import SimpleHTTPServer
-import SocketServer
+import http.server
+import socketserver
 import logging
 import cgi
 import ssl
@@ -13,30 +13,32 @@ PORT = 443
 ssl_key_file = "<PATH TO KEY FILE>"
 ssl_certificate_file = "<PATH TO Cert>"
 
-class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+class ServerHandler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
         logging.error(self.headers)
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
         logging.error(self.headers)
         form = cgi.FieldStorage(
             fp=self.rfile,
             headers=self.headers,
-            environ={'REQUEST_METHOD':'POST',
-                     'CONTENT_TYPE':self.headers['Content-Type'],
+            environ={'REQUEST_METHOD': 'POST',
+                     'CONTENT_TYPE': self.headers['Content-Type'],
                      })
         for item in form.list:
             logging.error(item)
-        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+        http.server.SimpleHTTPRequestHandler.do_GET(self)
+
 
 Handler = ServerHandler
 
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-httpd.socket = ssl.wrap_socket (httpd.socket, server_side=True,
-                                keyfile=ssl_key_file,
-                                certfile=ssl_certificate_file)
+httpd = socketserver.TCPServer(("", PORT), Handler)
+httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True,
+                               keyfile=ssl_key_file,
+                               certfile=ssl_certificate_file)
 
-print "serving at port", PORT
+print(f"serving at port: {PORT}")
 httpd.serve_forever()
